b17fe17e2c5923eef12c75a6882f9c222dc5eb89
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-05-09 23:57:42 +0530

    Update app.py

--- covidoff-data-portal/app.py
+++ covidoff-data-portal/app.py
@@ -18,7 +18,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    'covidoff-ecef33b9fe0b.json', scope)
+    'file_name_cant_be_shared_due_to_privacy_issues.json', scope)
 gc = gspread.authorize(credentials)
 st.write("# **CovidOff View Data Portal**")
 st.write("This is the Portal for all the Data Resources. These resources are filled by general people. Verification at utmostlevel is not guaranteed. Select your required service from the below dropdown")
